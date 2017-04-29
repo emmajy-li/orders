ba90a6dbcac66b28de565f2781229253ed134ee8
李静怡 <wwwmipp@sina.com>
2017-04-29 14:32:17 +0800

    Initial Commit

---
+++ class_orders/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  class_orders
+//
+//  Created by 李静怡 on 4/29/17.
+//  Copyright © 2017 李静怡. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
